af711f23842df4139fbdbd3ef032f72107f23b92
FeihongChuSecE <faye.chu.fh@gmail.com>
2025-09-27 12:09:17 -0400

    add basic routes

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const app = express();
-const PORT = 4000;
 
-//router
-app.get("/", (req, res, next) => {
-  res.send("Hello, welcome to your movie rescored");
+
+
+//Home page: navigation to other page
+//get all movies
+app.get("/movies", (req, res, next) => {
+  res.send("Hello, welcome to your movie page");
 })
 
+//get a book by id, name, actors/actress
+app.get("/movies/:id", (req, res, next) => {
+  res.send("Hello, welcome to your movie page");
+})
+
+//add a movie 
+app.post("/movies", (req, res, next) => {
+  res.send("Hey you watched a new movie");
+})
+
+//update data if the user want to change the view
+app.put("/movies/:id", (req, res, next) => {
+  res.send("Hey you watched a new movie");
+})
+
+//deleate a movie
+app.delete("/movies/:id", (req, res, next) => {
+  res.send("Hey you watched a new movie");
+})
+
+
+//starting the server
+const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server runing at http://localhost:${PORT}`);
 })
